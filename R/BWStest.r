e8f3ae258bbd629d6988416e0e700c017030c012
Steven E. Pav <shabbychef@gmail.com>
2018-10-18 10:40:11 +0000

    version 0.2.2

--- R/BWStest.r
+++ R/BWStest.r
@@ -64,6 +64,11 @@ NULL
 #' \newcommand{\cranBWStest}{\CRANpkg{BWStest}}
 #' \newcommand{\BWStest}{\href{https://github.com/shabbychef/BWStest}}
 #'
+#' @section \BWStest{} Version 0.2.2 (2018-10-17) :
+#' \itemize{
+#' \item Package maintenance--no new features.
+#' }
+#'
 #' @section \BWStest{} Version 0.2.1 (2017-03-20) :
 #' \itemize{
 #' \item Package maintenance--no new features.
